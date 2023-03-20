a63915e91929b2c81bb22ecf6e078e4bf5ac827d
tayyabansari <mtayyabansari30@gmail.com>
2023-03-20 18:52:57 +0500

    update signup

--- resources/views/front/vendor_signup.blade.php
+++ resources/views/front/vendor_signup.blade.php
@@ -1,6 +1,15 @@
 @extends('front.layouts.master')
 @section('content')
 
+<style>
+    .select2-container .select2-selection--single{height: 50px;}
+    .select2-container .select2-selection--single .select2-selection__rendered{line-height: 50px;padding-left: 15px;}
+    .select2-container--default .select2-selection--single .select2-selection__arrow{top: 12px;right: 5px;}
+    .select2-container .select2-selection--multiple .select2-selection__rendered{display: block;}
+    .select2-search__field{min-height: 45px !important;margin: 0 !important;padding-left: 10px !important;padding-top: 15px !important;}
+    .select2-selection__rendered{margin-bottom: 0;}
+</style>
+
 <div class="banner_main">
     <div class="slider-content pb-5">
         <h1>Create A New Account?</h1>
@@ -27,6 +36,12 @@
                                 <label for="email" class="form-label fw-500">Email</label>
                                 <input type="email" class="form-control" id="email" name="email" placeholder="Email" value="" >
                             </div>
+
+                            <div class="mb-4">
+                                <label for="contact" class="form-label fw-500">Contact No</label>
+                                <input type="text" class="form-control" id="contact" name="contact" placeholder="Contact No" value="" >
+                            </div>
+
                             <div class="mb-4">
                                 <label for="email" class="form-label fw-500">Country</label>
                                 <select class="form-control select_2_class" name="country_id" id="country_id">
@@ -48,17 +63,34 @@
                             <div class="mb-4">
                                 <div class="col-12">
                                     <div class="row">
+
                                         <div class="col-md-6">
                                             <label for="pass" class="form-label fw-500">Password</label>
-                                            <input type="password" class="form-control" id="pass" name="password" placeholder="Enter Password" >
+                                            <div class="input-group mb-3">
+                                                <input type="password" class="form-control password" id="pass" name="password" placeholder="Enter Password" >
+                                                <button type="button" class="input-group-text btn-theme" id="basic-addon2" onclick="hiden_show_password()">
+                                                    <i class="fas fa-eye"></i>
+                                                </button>
+                                            </div>
                                         </div>
 
                                         <div class="col-md-6">
                                             <label for="cpass" class="form-label fw-500">Confirm Password</label>
-                                            <input type="password" class="form-control" id="cpass" name="password_confirmation" placeholder="Confirm Password" >
+
+                                            <div class="input-group mb-3">
+                                                <input type="password" class="form-control" id="cpass" name="password_confirmation" placeholder="Confirm Password" >
+                                                <button type="button" class="input-group-text btn-theme" id="basic-addon2" onclick="hiden_show_password()">
+                                                    <i class="fas fa-eye"></i>
+                                                </button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+
+                            <div class="mb-4">
+                                <label for="profile_image" class="form-label fw-500">Profile Image</label>
+                                <input type="file" class="form-control" name="profile_image" id="profile_image">
                             </div>
 
                             <button type="submit" class="btn btn-theme w-100">Sign Up</button>
